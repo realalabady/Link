ae2c0f92f774cae17052844449a7043255ece1e3
realalabady <fakealabady+1@gmail.com>
2026-01-30 21:01:07 +0300

    edit

--- src/pages/admin/AdminPayoutsPage.tsx
+++ src/pages/admin/AdminPayoutsPage.tsx
@@ -122,7 +122,7 @@ const AdminPayoutsPage: React.FC = () => {
   ) => {
     if (isLoading) {
       return (
-        <div className="space-y-4">
+        <div className="space-y-3">
           {[1, 2, 3].map((i) => (
             <Skeleton key={i} className="h-24 w-full rounded-xl" />
           ))}
